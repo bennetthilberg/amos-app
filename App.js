eb03cb34e0dac719c71c3105787d7d2c6607d044
bennetthilberg <bennett.hilberg@gmail.com>
2023-07-26 10:53:01 -0500

    Set up up to importing jotai

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Ionicons } from '@expo/vector-icons';
+import {Button} from 'react-native-elements';
+import { Provider } from 'jotai';
+
+const Stack = createStackNavigator();
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button 
+      title={'Press me!'}>
+
+      </Button>
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
