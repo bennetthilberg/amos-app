8e25fb30e1bea62a1c7843b2ce505180ebc4de8e
bennetthilberg <bennett.hilberg@gmail.com>
2023-07-31 14:07:11 -0500

    Add big raceIAT.js, add faces to assets/faces

---
+++ Attributions.js
@@ -0,0 +1,16 @@
+/*
+
+FairFace: Face Attribute Dataset for Balanced Race, Gender, and Age
+@inproceedings{karkkainenfairface,
+      title={FairFace: Face Attribute Dataset for Balanced Race, Gender, and Age for Bias Measurement and Mitigation},
+      author={Karkkainen, Kimmo and Joo, Jungseock},
+      booktitle={Proceedings of the IEEE/CVF Winter Conference on Applications of Computer Vision},
+      year={2021},
+      pages={1548--1558}
+}
+CC BY 4.0 license https://github.com/joojs/fairface
+
+
+
+
+*/
--- tests/data/raceIAT.js
+++ tests/data/raceIAT.js
@@ -1,78 +1,153 @@
+const goodWords = ['Joy', 'Love', 'Peace', 'Wonderful', 'Pleasure', 'Friend', 'Laughter', 'Happy'];
+const badWords = ['Agony', 'Terrible', 'Horrible', 'Nasty', 'Evil', 'War', 'Awful', 'Failure'];
+const blackFaces = ['face1.jpeg', 'face2.jpeg', 'face3.jpeg', 'face4.jpeg']; // replace with actual image paths
+const whiteFaces = ['face5.jpeg', 'face6.jpeg', 'face7.jpeg', 'face8.jpeg']; // replace with actual image paths
+
+const createWordQuestion = (word, isGood) => ({
+  type: 'practice',
+  questionText: 'Categorize this word:',
+  item: word,
+  answerTextA: 'Bad',
+  answerTextB: 'Good',
+  buttonAColor: '#FF0000',
+  buttonBColor: '#008000',
+  correctAnswer: isGood ? 'B' : 'A',
+});
+
+const createFaceQuestion = (face, isBlack) => ({
+  type: 'question',
+  questionText: 'Categorize this face:',
+  item: `./assets/faces/${face}`,
+  answerTextA: 'Black',
+  answerTextB: 'White',
+  buttonAColor: '#ffe0bd',
+  buttonBColor: '#260701',
+  correctAnswer: isBlack ? 'A' : 'B',
+});
+
 export const raceIAT = {
-    id: 2,
-    name: 'Race Implicit Association Test',
-    iconSrc: './assets/icons/race.svg',
-    takenYet: false,
-    questions: [
+  id: 2,
+  name: 'Race Implicit Association Test',
+  iconSrc: './assets/icons/race.svg',
+  takenYet: false,
+  blocks: [
+    // Block 1
+    {
+      instructions: [
         {
+          type: 'instruction',
+          questionText: 'Welcome to the Race IAT! You will be shown words and images and asked to categorize them.',
+          buttonLayout: 'oneBig',
+          buttonText: 'Start',
+        },
+        {
+          type: 'instruction',
+          questionText: 'In the first part, you will categorize faces. Tap "Black" for Black faces and "White" for White faces.',
+          buttonLayout: 'oneBig',
+          buttonText: 'Next',
+        },
+      ],
+      questions: [
+        ...blackFaces.map(face => createFaceQuestion(face, true)),
+        ...whiteFaces.map(face => createFaceQuestion(face, false)),
+      ],
+    },
+    // Block 2
+    {
+      instructions: [
+        {
+          type: 'instruction',
+          questionText: 'Next, you will categorize words. Tap the green button for good items, and the red button for bad items.',
+          buttonLayout: 'oneBig',
+          buttonText: 'Next',
+        },
+      ],
+      questions: [
+        ...goodWords.map(word => createWordQuestion(word, true)),
+        ...badWords.map(word => createWordQuestion(word, false)),
+      ],
+    },
+    // Block 3
+    {
+      instructions: [
+        {
+          type: 'instruction',
+          questionText: 'Now we will combine faces and words. The correct categories are: Black or Bad with the left button and White or Good with the right button.',
+          buttonLayout: 'oneBig',
+          buttonText: 'Next',
+        },
+      ],
+      questions: [
+        ...blackFaces.map(face => createFaceQuestion(face, true)),
+        ...whiteFaces.map(face => createFaceQuestion(face, false)),
+        ...goodWords.map(word => createWordQuestion(word, true)),
+        ...badWords.map(word => createWordQuestion(word, false)),
+      ],
+    },
+    // Block 4
+    {
+        instructions: [
+          {
             type: 'instruction',
-            questionText: 'Welcome to the Race IAT! You will be shown words and images and asked to categorize them.',
+            questionText: 'The categories for faces have now switched places. Now tap "Black" for Black faces and "White" for White faces.',
             buttonLayout: 'oneBig',
             buttonText: 'Next',
-        },
-        {
+          },
+        ],
+        questions: [
+          ...blackFaces.map(face => createFaceQuestion(face, true)),
+          ...whiteFaces.map(face => createFaceQuestion(face, false)),
+        ],
+    },
+    // Block 5
+    {
+        instructions: [
+          {
             type: 'instruction',
-            questionText: 'Tap the green button for good items, and the red button for bad items. We\'ll practice this next.',
-            image: './assets/diagrams/example_diagram.png',
+            questionText: 'Continue with the reversed categories.',
             buttonLayout: 'oneBig',
             buttonText: 'Next',
-        },
-        {
-            type: 'practice',
-            questionText: 'Categorize this item:',
-            item: 'Good',
-            answerTextA: 'Bad',
-            answerTextB: 'Good',
-            buttonAColor: '#FF0000',
-            buttonBColor: '#008000',
-            answeredYet: false,
-            response: null,
-            timeTaken: null
-        },
-        {
-            type: 'practice',
-            questionText: 'Categorize this item:',
-            item: 'Bad',
-            answerTextA: 'Bad',
-            answerTextB: 'Good',
-            buttonAColor: '#FF0000',
-            buttonBColor: '#008000',
-            answeredYet: false,
-            response: null,
-            timeTaken: null
-        },
-        {
+          },
+        ],
+        questions: [
+          ...blackFaces.map(face => createFaceQuestion(face, true)),
+          ...whiteFaces.map(face => createFaceQuestion(face, false)),
+        ],
+      },
+    // Block 6
+    {
+        instructions: [
+          {
             type: 'instruction',
-            questionText: 'Next, you will categorize faces. Tap "Tan" for Black faces and "Brown" for White faces.',
+            questionText: 'Now we will combine the reversed categories of faces and words. The correct categories are: Black or Good with the left button and White or Bad with the right button.',
             buttonLayout: 'oneBig',
             buttonText: 'Next',
-            image: './assets/diagrams/example_diagram2.png',
-        },
-        {
-            type: 'question',
-            questionText: 'Categorize this face:',
-            item: './assets/faces/face1.png',
-            answerTextA: 'Tan',
-            answerTextB: 'Brown',
-            buttonAColor: '#ffe0bd',
-            buttonBColor: '#260701',
-            answeredYet: false,
-            response: null,
-            timeTaken: null
-        },
-        {
-            type: 'question',
-            questionText: 'Categorize this face:',
-            item: './assets/faces/face2.png',
-            answerTextA: 'Tan',
-            answerTextB: 'Brown',
-            buttonAColor: '#ffe0bd',
-            buttonBColor: '#260701',
-            answeredYet: false,
-            response: null,
-            timeTaken: null
-        },
-        // ...
-    ],
-    progress: 0, // which question the user is on
+          },
+        ],
+        questions: [
+          ...blackFaces.map(face => createFaceQuestion(face, true)),
+          ...whiteFaces.map(face => createFaceQuestion(face, false)),
+          ...goodWords.map(word => createWordQuestion(word, true)),
+          ...badWords.map(word => createWordQuestion(word, false)),
+        ],
+      },
+    // Block 7
+    {
+        instructions: [
+          {
+            type: 'instruction',
+            questionText: 'For the final task, the categories will switch back to their original places. Now, the correct categories are: Black or Bad with the left button and White or Good with the right button.',
+            buttonLayout: 'oneBig',
+            buttonText: 'Next',
+          },
+        ],
+        questions: [
+          ...blackFaces.map(face => createFaceQuestion(face, true)),
+          ...whiteFaces.map(face => createFaceQuestion(face, false)),
+          ...goodWords.map(word => createWordQuestion(word, true)),
+          ...badWords.map(word => createWordQuestion(word, false)),
+        ],
+    },
+  ],
+  progress: 0, // which question the user is on
 }
